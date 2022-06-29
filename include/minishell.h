9223877f5e66953015b4d8354f5b8a02c2a07638
hmigl <88746406+hmigl@users.noreply.github.com>
2022-06-29 10:15:36 -0300

    Refactor minishell header
    This commit include some changes on how structs were
    organized. Style changes were also applied.
    Global var `g_ms` is included here with external keyword.

--- include/minishell.h
+++ include/minishell.h
@@ -9,28 +9,28 @@
 # include <readline/history.h>
 # include <fcntl.h>
 
-typedef struct s_var{
-	char			*key;
-	char			*value;
-	struct s_var	*next;
-}	t_var;
+typedef struct s_var		t_var;
+typedef struct s_minishell	t_minishell;
 
-typedef struct s_minishell
-{
+extern t_minishell			g_ms;
+
+struct s_var {
+	char	*key;
+	char	*value;
+	t_var	*next;
+};
+
+struct s_minishell {
 	t_var	*env_var;
-}	t_minishell;
-
-t_minishell	*g_global;
-
+};
 
 // FUNCTIONS
-
-void	ft_save_local_env (char **env);
-void	ft_start_shell (void);
-void	ft_redirect (char *prompt_line);
-char	*ft_in_redirect (char *str, char *prompt_line);
-int		ft_next_occurrence (char *str, char y);
-char	*ft_file_name (char *prompt_line, char redirect);
-char	*ft_trim_redirect (char *str);
+void	ft_save_local_env(char **env);
+void	ft_start_shell(void);
+void	ft_redirect(char *prompt_line);
+char	*ft_in_redirect(char *str, char *prompt_line);
+int		ft_next_occurrence(char *str, char y);
+char	*ft_file_name(char *prompt_line, char redirect);
+char	*ft_trim_redirect(char *str);
 
 #endif // MINISHELL_H
