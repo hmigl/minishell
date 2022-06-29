2f9fcbda743eb6c8152804532f3eba39046bbeaf
hmigl <88746406+hmigl@users.noreply.github.com>
2022-06-29 10:19:42 -0300

    Add some functions
    A function that removes a single node from a struct was
    added, as well as a new version of `insert_nodes_in_struct`

--- src/save_env_var/save_local_env.c
+++ src/save_env_var/save_local_env.c
@@ -1,56 +1,81 @@
 #include "minishell.h"
 
-//static void	rm_single_node(t_var *node){}
-
-static void ft_new_node (char *key, char *value, t_var *anchor)
+static void	rm_single_node(t_var *node)
 {
-	t_var *new_node;
-
-	new_node = (t_var *) ft_calloc (1, sizeof(t_var));
-	new_node->value = value;
-	new_node->key = key;
-	if (!g_global->env_var->value)
-	{
-		free (g_global->env_var);
-		g_global->env_var = new_node;
-		return;
-	}
-	anchor = g_global->env_var;
-	while (anchor->next)
-		anchor = anchor->next;
-	anchor->next = new_node;
-	return;
+	free(node->key);
+	free(node->value);
+	free(node);
 }
-static void ft_insert_nodes_in_struct (char *key, char *value)
+
+// static void ft_new_node (char *key, char *value, t_var *anchor)
+// {
+// 	t_var *new_node;
+//
+// 	new_node = (t_var *) ft_calloc (1, sizeof(t_var));
+// 	new_node->value = value;
+// 	new_node->key = key;
+// 	if (!g_global.env_var->value)
+// 	{
+// 		free (g_global.env_var);
+// 		g_global.env_var = new_node;
+// 		return;
+// 	}
+// 	anchor = g_global.env_var;
+// 	while (anchor->next)
+// 		anchor = anchor->next;
+// 	anchor->next = new_node;
+// 	return;
+// }
+
+// static void ft_insert_nodes_in_struct (char *key, char *value)
+// {
+// 	t_var *anchor;
+// 	//printf  ("global= %s\n",g_global->env_var);
+// 	anchor = g_global->env_var;
+// 	while (anchor)
+// 	{
+// 		if (!(ft_strcmp (anchor->key, key)))
+// 		{
+// 			free (anchor->value);
+// 			free (key);
+// 			anchor->value = value;
+// 			return ;
+// 		}
+// 		anchor = anchor->next;
+// 	}
+// 	ft_new_node (key, value, anchor);
+// 	return;
+// }
+
+static void ft_insert_nodes_in_struct(char *key, char *value)
 {
 	t_var *anchor;
-	//printf  ("global= %s\n",g_global->env_var);
-	anchor = g_global->env_var;
-	while (anchor)
+	t_var *new_node;
+
+	anchor = g_ms.env_var;
+	new_node = ft_calloc(sizeof(t_var), 1);
+	new_node->key = key;
+	new_node->value = value;
+	if (anchor == NULL)
+		g_ms.env_var = new_node;
+	else
 	{
-		if (!(ft_strcmp (anchor->key, key)))
-		{
-			free (anchor->value);
-			free (key);
-			anchor->value = value;
-			return ;
-		}
-		anchor = anchor->next;
+		while (anchor->next)
+			anchor = anchor->next;
+		anchor->next = new_node;
 	}
-	ft_new_node (key, value, anchor);
-	return;
 }
 
-void ft_save_local_env (char **env)
+void ft_save_local_env(char **env)
 {
-	int i;
-	char **splited;
+	int		i;
+	char	**splited;
 
 	i = 0;
 	while (env[i])
 	{
 		splited = ft_split(env[i], '=');
-		ft_insert_nodes_in_struct (splited[0], splited[1]);
+		ft_insert_nodes_in_struct(splited[0], splited[1]);
 		i++;
 	}
 	return ;
