1ef6bfdae2863933d98007fe3944ec0f4457bfaf
hmigl <88746406+hmigl@users.noreply.github.com>
2022-06-30 08:35:56 -0300

    Clean up file; apply norm

--- src/env/save_local_env.c
+++ src/env/save_local_env.c
@@ -1,50 +1,16 @@
 #include "minishell.h"
 
-static void	rm_single_node(t_var *node)
+void	rm_single_node(t_var *node)
 {
 	free(node->key);
 	free(node->value);
 	free(node);
 }
 
-// static void ft_new_node (char *key, char *value, t_var *anchor)
-// {
-// 	t_var *new_node;
-//
-// 	new_node = (t_var *) ft_calloc (1, sizeof(t_var));
-// 	new_node->value = value;
-// 	new_node->key = key;
-// 	if (!g_global.env_var->value)
-// 	{
-// 		free (g_global.env_var);
-// 		g_global.env_var = new_node;
-// 		return;
-// 	}
-// 	anchor = g_global.env_var;
-// 	while (anchor->next)
-// 		anchor = anchor->next;
-// 	anchor->next = new_node;
-// 	return;
-// }
-
-// static void ft_insert_nodes_in_struct (char *key, char *value)
-// {
-// 	t_var *anchor;
-// 	//printf  ("global= %s\n",g_global->env_var);
-// 	anchor = g_global->env_var;
-// 	while (anchor)
-// 	{
-
-// 		anchor = anchor->next;
-// 	}
-// 	ft_new_node (key, value, anchor);
-// 	return;
-// }
-
-static void ft_insert_nodes_in_struct(char *key, char *value)
+static void	ft_insert_nodes_in_struct(char *key, char *value)
 {
-	t_var *anchor;
-	t_var *new_node;
+	t_var	*anchor;
+	t_var	*new_node;
 
 	anchor = g_ms->env_var;
 	new_node = ft_calloc(sizeof(t_var), 1);
@@ -56,7 +22,7 @@ static void ft_insert_nodes_in_struct(char *key, char *value)
 	{
 		while (anchor->next)
 		{
-	 		if (!(ft_strcmp (anchor->key, key)))
+			if (!(ft_strcmp (anchor->key, key)))
 			{
 				free (anchor->value);
 				free (key);
@@ -69,7 +35,7 @@ static void ft_insert_nodes_in_struct(char *key, char *value)
 	}
 }
 
-void ft_save_local_env(char **env)
+void	ft_save_local_env(char **env)
 {
 	int		i;
 	char	**splited;
