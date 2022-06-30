ece49baa4bd3331aff8b61913b07074f43ddb93a
hmigl <88746406+hmigl@users.noreply.github.com>
2022-06-30 10:33:49 -0300

    Add error management function

---
+++ src/shell/ms_display_error.c
@@ -0,0 +1,10 @@
+#include "minishell.h"
+
+void	ms_display_error(char *id, char *err, int should_quit)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd(id, STDERR_FILENO);
+	ft_putendl_fd(err, STDERR_FILENO);
+	if (should_quit)
+		exit(g_ms->exit_code);
+}
--- src/shell/prompt.c
+++ src/shell/prompt.c
@@ -1,9 +1,9 @@
 #include "minishell.h"
 
-static void ft_alloc_cmd (char* str)
+static void	ft_alloc_cmd(char *str)
 {
-	int i;
-	int alloc_cmd;
+	int	i;
+	int	alloc_cmd;
 
 	alloc_cmd = 2;
 	i = 0;
@@ -16,47 +16,46 @@ static void ft_alloc_cmd (char* str)
 		i++;
 	}
 	i = 0;
-	g_ms->cmd_node = ft_calloc(alloc_cmd, sizeof(t_cmd*));
+	g_ms->cmd_node = ft_calloc(alloc_cmd, sizeof(t_cmd));
 	while (alloc_cmd--)
 		g_ms->cmd_node[i++] = ft_calloc(alloc_cmd, sizeof(t_cmd));
-
 }
 
-
-static void ft_save_history (char *prompt_line)
+static void	ft_save_history(char *prompt_line)
 {
 	if (*prompt_line)
-		add_history (prompt_line);
+		add_history(prompt_line);
 	return ;
 }
 
-static char *ft_prompt (void)
+static char	*ft_prompt(void)
 {
-	char *str;
-	char *cwd;
+	char	*str;
+	char	*cwd;
 
-	cwd = getcwd (0, 0);
-	printf ("%s", cwd);
+	cwd = getcwd(NULL, 0);
+	printf("%s", cwd);
 	str = readline("/Minishell> ");
-	free (cwd);
+	free(cwd);
 	if (!str[0])
 	{
-		free (str);
-		ft_prompt ();
+		free(str);
+		ft_prompt();
 	}
 	return (str);
 }
 
-void	ft_start_shell (void)
+void	ft_start_shell(void)
 {
-	char *prompt_line;
+	char	*prompt_line;
+
 	while (1)
 	{
 		g_ms->pipe = 0;
-		prompt_line = ft_prompt ();
+		prompt_line = ft_prompt();
 		ft_save_history(prompt_line);
-		ft_alloc_cmd (prompt_line);
-		prompt_line = ft_redirect (prompt_line);
-		printf ("%s\n", prompt_line);
+		ft_alloc_cmd(prompt_line);
+		prompt_line = ft_redirect(prompt_line);
+		printf("%s\n", prompt_line);
 	}
 }
