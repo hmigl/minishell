c458a2f7dd969a834d1d1a08d7e2bd73112d2951
hmigl <88746406+hmigl@users.noreply.github.com>
2022-08-15 11:05:44 -0300

    feat: make prompt like bash's

--- src/shell/prompt.c
+++ src/shell/prompt.c
@@ -1,4 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prompt.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmigl <hmigl@student.42sp.org.br>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/15 10:26:29 by hmigl             #+#    #+#             */
+/*   Updated: 2022/08/15 10:58:11 by hmigl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+#define YELLOW "\001\033[1;33m\002"
+#define GREEN "\001\033[1;32m\002"
+#define BLUE "\001\033[1;34m\002"
+#define MAGENTA_LIGHT "\001\033[1;95m\002"
+#define RESET "\001\033[0;0m\002"
 
 void	ft_alloc_cmd(char *str)
 {
@@ -22,18 +40,17 @@ void	ft_alloc_cmd(char *str)
 
 int	ft_save_history(char *prompt_line)
 {
-	int i;
-	int only_space;
+	int	i;
+	int	only_space;
 
 	i = -1;
 	only_space = 1;
-
 	while (prompt_line[++i])
 	{
 		if (prompt_line[i] != ' ')
 		{
 			only_space--;
-			break;
+			break ;
 		}
 	}
 	if (!only_space)
@@ -44,18 +61,54 @@ int	ft_save_history(char *prompt_line)
 	return (0);
 }
 
+static char	*getcurdir(void)
+{
+	char	*cwd;
+	char	**cwd_matriz;
+	int		i;
+
+	cwd = getcwd(NULL, 0);
+	cwd_matriz = ft_split(cwd, '/');
+	ft_free(cwd);
+	i = 0;
+	while (cwd_matriz[i] != NULL)
+	{
+		if (cwd_matriz[i + 1] == NULL)
+		{
+			cwd = ft_strdup(cwd_matriz[i]);
+			break ;
+		}
+		i++;
+	}
+	ft_free_double_pointer(cwd_matriz);
+	return (cwd);
+}
+
+static char	*format_prompt(void)
+{
+	char	*prompt;
+	char	*current_dir;
+	char	*buffer;
+	char	*tmp;
+
+	buffer = ft_strjoin(getenv("USER"), "@minishell ");
+	tmp = getcurdir();
+	current_dir = ft_strjoin(tmp, "$ ");
+	prompt = ft_strjoin(buffer, current_dir);
+	ft_free(tmp);
+	ft_free(current_dir);
+	ft_free(buffer);
+	return (prompt);
+}
+
 char	*ft_prompt(void)
 {
 	char	*buffer;
-	char	*path;
-	char	*minishell_path;
+	char	*ms_prompt;
 
-	path = NULL;
-	path = getcwd(path, 0);
-	minishell_path = ft_strjoin (path, "/Minishell>");
-	buffer = readline(minishell_path);
-	ft_free (minishell_path);
-	ft_free (path);
+	ms_prompt = format_prompt();
+	buffer = readline(ms_prompt);
+	free(ms_prompt);
 	if (buffer == NULL)
 	{
 		ft_putendl_fd("exit", STDERR_FILENO);
